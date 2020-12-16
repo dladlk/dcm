6b995131b658843efe515db263e3c8f7ed1945d9
Dmitriy Lapko <dlk@truelink.dk>
2020-12-16 17:02:59 +0200

    Add more details to about

--- cm-frontend/src/components/Banner.js
+++ cm-frontend/src/components/Banner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import CloseIcon from '@material-ui/icons/Close';
-import { Button, Card, CardContent, CardHeader, IconButton } from '@material-ui/core';
+import { Box, Button, Card, CardContent, CardHeader, IconButton } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -29,20 +29,35 @@ export default function Banner(props) {
               }
             >
             </CardHeader>
-            <CardContent style={{ }}>
-                Here you can search for published products, delivered via Peppol in Catalogue transaction 3.1 format by different sellers, and find aggregated information by standard identifier for next concerns:
-                <ul>
-                  <li>product descriptions</li>
-                  <li>classification</li>
-                  <li>environmental labelling</li>
-                  <li>social responsibility</li>
-                  <li>ecological labelling</li>
-                  <li>pictures</li>
-                  <li>country of origin</li>
-                </ul>
+            <CardContent>
+                <div>
+                  Here you can search for published products, delivered via Peppol in Catalogue transaction 3.1 format by different sellers,
+                  and find aggregated information by standard identifiers.
+                </div>
+              <Box display="flex" justifyContent="left" mt={3}>
+                <Box flex="1"  maxWidth = "500px">
+                  Aggregated information includes master data about:
+                    <ul>
+                    <li>product descriptions</li>
+                    <li>classification</li>
+                    <li>environmental and ecological labelling</li>
+                    <li>social responsibility</li>
+                    <li>pictures</li>
+                    <li>country of origin</li>
+                  </ul>
+                </Box>
+                <Box flex="1" maxWidth = "500px">
+                  Solution includes only master data, and does not include sellers-specific information about:
+                    <ul>
+                    <li>prices</li>
+                    <li>content and orderable units</li>
+                    <li>related items</li>
+                  </ul>
+                </Box>
+              </Box>
               <Button size="small" color="primary" variant="outlined" onClick={() => { closeAction() }}>
-                Hide
-              </Button>
+                Hide welcome dialog
+                </Button>
             </CardContent>
           </Card>
         )}
