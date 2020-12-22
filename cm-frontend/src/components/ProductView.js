28ad13495ab4f1e4e6669331f1e988dcfa20f4fc
Dmitriy Lapko <dlk@truelink.dk>
2020-12-22 12:16:33 +0200

    Simplify Quantity by passing renderValue param

--- cm-frontend/src/components/ProductView.js
+++ cm-frontend/src/components/ProductView.js
@@ -11,33 +11,33 @@ const useStyles = makeStyles(theme => ({
     name: {
         fontWeight: 'bold',
         flex: '1',
-        minWidth: '80px', 
+        minWidth: '80px',
     },
     value: {
         flex: '6',
     }
-  }));
+}));
 
-function DataView (props) {
+function DataView(props) {
 
     const _isValueDefined = (value) => value ? true : false;
     const _renderValue = (v, i) => { return v };
-    
-    const {name, value, isValueDefined = _isValueDefined, renderValue = _renderValue} = props;
+
+    const { name, value, isValueDefined = _isValueDefined, renderValue = _renderValue } = props;
     const classes = useStyles();
 
-    return ( 
+    return (
         <> {isValueDefined(value) ? (
-        <div className={classes.row}>
-            <div className={classes.name}>{name}</div>
-            <div className={classes.value}>{renderValue(value)}</div>
-        </div>
-        ) : ( <></> ) } 
+            <div className={classes.row}>
+                <div className={classes.name}>{name}</div>
+                <div className={classes.value}>{renderValue(value)}</div>
+            </div>
+        ) : (<></>)}
         </>
     )
 }
 
-function DataListView (props) {
+function DataListView(props) {
     const _isValueDefined = (value) => value && value.length > 0 ? true : false;
     const _renderListValue = (v, i) => {
         return (
@@ -46,47 +46,42 @@ function DataListView (props) {
             </Fragment>
         )
     }
-    const {name, value, isValueDefined = _isValueDefined, renderListValue = _renderListValue} = props;
-    return ( 
-        <DataView name={name} value = {value} isValueDefined = {isValueDefined} renderValue={(value) => value.map(renderListValue)} />
+    const { name, value, isValueDefined = _isValueDefined, renderListValue = _renderListValue } = props;
+    return (
+        <DataView name={name} value={value} isValueDefined={isValueDefined} renderValue={(value) => value.map(renderListValue)} />
     )
 }
 
-function Quantity (props) {
-    const {name, value} = props;
-    function quantityValue(quantity) {
-        return (
-            <>{quantity.quantity} {quantity.unitCode}</>
-        )
-    }
+function Quantity(props) {
+    const renderQuantity = (quantity) => { return (<>{quantity.quantity} {quantity.unitCode}</>) };
     return (
-        <> {value ? (
-            <DataView name = {name} value={quantityValue(value)}></DataView>
-        ) : ( <></> ) } 
-        </>
+        <DataView name={props.name} value={props.value} renderValue={renderQuantity} />
     )
 }
+
+
+const renderUrl = (v) => { return (<a href={v} rel="noreferrer" target="_blank">{v}</a>) };
 
 export default function ProductView(props) {
 
     const showTech = false;
 
-    const {product} = props;
+    const { product } = props;
 
     return (
         <>
             { showTech ? (
                 <>
-                <DataView name="ID" value={product.id}></DataView>
-                <DataView name="Created" value={product.createTime}></DataView>
-                <DataView name="Updated" value={product.updateTime}></DataView>
-                <DataView name="Version" value={product.version}></DataView>
-                <DataView name="Orderable Indicator" value={product.document.orderableIndicator}></DataView>
-                <Quantity name="Content Quantity" value={product.document.contentUnitQuantity}></Quantity>
-                <Quantity name="Minimum Quantity" value={product.document.minimumOrderQuantity}></Quantity>
-                <Quantity name="Maximum Quantity" value={product.document.maximumOrderQuantity}></Quantity>
+                    <DataView name="ID" value={product.id}></DataView>
+                    <DataView name="Created" value={product.createTime}></DataView>
+                    <DataView name="Updated" value={product.updateTime}></DataView>
+                    <DataView name="Version" value={product.version}></DataView>
+                    <DataView name="Orderable Indicator" value={product.document.orderableIndicator}></DataView>
+                    <Quantity name="Content Quantity" value={product.document.contentUnitQuantity}></Quantity>
+                    <Quantity name="Minimum Quantity" value={product.document.minimumOrderQuantity}></Quantity>
+                    <Quantity name="Maximum Quantity" value={product.document.maximumOrderQuantity}></Quantity>
                 </>
-            ) : ( <></> ) }
+            ) : (<></>)}
 
             <DataView name="Name" value={product.document.item.name}></DataView>
             <DataView name="Standard number" value={ItemDetailsService.itemStandardNumber(product.document.item)}></DataView>
@@ -95,7 +90,7 @@ export default function ProductView(props) {
             <DataListView name="Keywords" value={product.document.item.keywordList}></DataListView>
             <DataView name="UNSPSC" value={ItemDetailsService.itemUNSPSC(product.document.item)}></DataView>
             <DataView name="Origin Country" value={ItemDetailsService.itemOriginCountry(product.document.item)}></DataView>
-            <DataView name="URL" value={ItemDetailsService.itemPictureURL(product.document.item)}></DataView>
+            <DataView name="Picture" value={ItemDetailsService.itemPictureURL(product.document.item)} renderValue={renderUrl} />
             <DataListView name="Certificates" value={ItemDetailsService.itemCertificates(product.document.item)} renderListValue={ItemDetailsService.renderItemCertificate}></DataListView>
         </>
     )
