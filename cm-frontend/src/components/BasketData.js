fa8c8732f89f6a7c49076cbfd1666418fa0e2268
Dmytro Lapko <dlk@mercell.com>
2021-12-01 13:11:18 +0200

    Use external class BasketData to manage basket state and reference it in React.useState

--- cm-frontend/src/components/BasketData.js
+++ cm-frontend/src/components/BasketData.js
@@ -1,13 +1,30 @@
 export function createBasketData() {
 
     class BasketData {
-        constructor() {
-            this.orderLines = {};
-            this.orderLinesCount = 0;
+        constructor(orderLines = {}, orderLinesCount = 0) {
+            this.orderLines = orderLines;
+            this.orderLinesCount = orderLinesCount;
         }
 
         isEmpty() {
-            return this.orderLines.length === 0;
+            return this.orderLinesCount === 0;
+        }
+
+        changeBasket(productId, quantity) {
+            let newOrderLines = {...this.orderLines};
+            let newOrderLinesCount = this.orderLinesCount;
+            if (productId in newOrderLines) {
+                if (quantity > 0) {
+                    newOrderLines[productId] += quantity;
+                } else {
+                    delete newOrderLines[productId]
+                    newOrderLinesCount--;
+                }
+            } else {
+                newOrderLines[productId] = quantity;
+                newOrderLinesCount++;
+            }
+            return new BasketData(newOrderLines, newOrderLinesCount);
         }
     }
 
