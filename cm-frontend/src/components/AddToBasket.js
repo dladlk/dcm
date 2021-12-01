c8093cb27ebdbba7583fb0316dfe35a830766988
Dmytro Lapko <dlk@mercell.com>
2021-12-01 11:58:09 +0200

    AddToBasket - use enums with state, change button name on state change, show progress on adding

--- cm-frontend/src/components/AddToBasket.js
+++ cm-frontend/src/components/AddToBasket.js
@@ -1,17 +1,44 @@
 import {Button} from "@material-ui/core";
 import React, {useEffect, useRef} from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+
+const ProductBasketStatus = {
+    Empty: 'empty',
+    Adding: 'adding',
+    Added: 'added',
+}
 
 export default function AddToBasket() {
 
-    const [state, setState] = React.useState('empty');
+    const [state, setState] = React.useState(ProductBasketStatus.Empty);
 
+    const getButtonTitle = () => {
+        switch (state) {
+            case ProductBasketStatus.Adding:
+                return 'Adding to basket';
+            case ProductBasketStatus.Added:
+                return 'Remove from basket';
+            default:
+                return 'Add to basket';
+        }
+    }
+
+    const isProgress = () => {
+        return state === ProductBasketStatus.Adding;
+    }
+
+    // TODO: Remove - temporary code to imitate slow adding
     const timerRef = useRef(null);
-
-    function handleAdd() {
-        setState('adding');
-        timerRef.current = setTimeout(() => {
-            setState('added')
-        }, 500);
+    const handleClick = () => {
+        if (state === ProductBasketStatus.Empty) {
+            setState(ProductBasketStatus.Adding);
+            timerRef.current = setTimeout(() => {
+                setState(ProductBasketStatus.Added)
+            }, 800);
+        } else if (state === ProductBasketStatus.Added) {
+            setState(ProductBasketStatus.Empty);
+        }
     }
 
     useEffect(() => {
@@ -20,7 +47,12 @@ export default function AddToBasket() {
 
     return (
         <>
-            <Button variant="outlined" size={"small"} color="primary" onClick={() => handleAdd()}>Add to basket</Button>
+            <Button variant="outlined" size={"small"} color="primary" onClick={() => handleClick()}>
+                {isProgress() && (
+                    <CircularProgress size={'1rem'} style={{marginRight: '5px'}}/>
+                )}
+                {getButtonTitle()}
+            </Button>
         </>
     )
 }
